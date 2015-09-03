67dc036ccd4d619f42083a93b424db2668b2dd9e
cmcclatchey <cmcclatchey@e112-m-08.mchenry.edu>
2015-09-03 16:14:16 -0500

    Money

---
+++ Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+println("-----------------------------------------------------------------------")
+println("|                                                                                               |")
+println("|                                                                                               |")
+println("|             / |                                                          / |                  |")
+println("|           /   |                        $      $                      /   |                  |")
+println("|               |                                                            |                  |")
+println("|     $        |                                                  $        |                  |")
+println("|               |                     $             $                      |                  |")
+println("|               |                       $         $                        |                  |")
+println("|          --------                     $  $                       --------             |")
+println("|                                                                                               |")
+println("|                                                                                               |")
+println("-----------------------------------------------------------------------")
